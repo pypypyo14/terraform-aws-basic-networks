eb27fec963338045962449a546fc5a53c4e22fa1
Saori <jkmr9214@gmail.com>
2019-09-28 19:59:50 +0900

    fix variables.tf

--- variables.tf
+++ variables.tf
@@ -2,8 +2,12 @@ variable "project" {
   default = "terraform_practice"
 }
 variable "vpc_cidr" {
-  default = "10.0.0.0/8"
+  default = "10.20.0.0/16"
 }
 variable "public_cidr" {
-  default = ["10.1.0.0/16", "10.2.0.0/16"]
+  default = ["10.20.0.0/20", "10.20.16.0/20"]
+}
+
+variable "private_cidr" {
+  default = ["10.20.48.0/20", "10.20.64.0/20"]
 }
